cdf163e35a7abaed3bd3e1c28558277394a91858
OllieMartin <olivermartin410@yahoo.co.uk>
2017-05-17 19:54:27 +0100

    More work on order manager

--- SushiSystem/src/business/OrderManager.java
+++ SushiSystem/src/business/OrderManager.java
@@ -15,6 +15,28 @@ public class OrderManager {
 		userOrders = new HashMap<String,List<Order>>();
 	}
 	
+	public void createOrder(String user, List<OrderDish> dishes) {
+		Order order = new Order(user, dishes);
+		orders.add(order);
+		if (userOrders.containsKey(user)) {
+			getUserOrders(user).add(order);
+		} else {
+			List<Order> newEntry = new ArrayList<Order>();
+			newEntry.add(order);
+			userOrders.put(user, newEntry);
+		}
+	}
+	
+	public List<Order> getWaitingOrders() {
+		List<Order> waiting = new ArrayList<Order>();
+		for (Order o : orders) {
+			if (o.getStatus() == OrderStatus.PLACED) {
+				waiting.add(o);
+			}
+		}
+		return waiting;
+	}
+	
 	public List<Order> getOrders() {
 		return this.orders;
 	}
